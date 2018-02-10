b6ec2feb1c1b58a313a51a41d98d01ae3cc453b1
Ivan Szebenszki <szib@users.noreply.github.com>
2018-02-10 19:46:25 +0000

    Add create article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
 end
